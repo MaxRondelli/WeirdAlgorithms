3ed48e89e0fade1b3cba3464291d131bf22ae3b2
MaxRondelli <massi.rondelli@gmail.com>
2023-08-27 13:09:16 +0200

    Add TwoSum.cpp algorithm

---
+++ TwoSum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * Example 1:
+ * Iput: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+class Solution
+{
+public:
+
+    /**
+     * Not actually the more efficient algorithm. It is O(n^2);
+     */
+    vector<int> twoSumBruteForce(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
